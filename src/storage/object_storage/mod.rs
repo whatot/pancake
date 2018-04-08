8cafcb5ae00181b14d7f971ffa5dfde74b5af11c
whatot <whatot2@gmail.com>
2018-04-09 00:35:31 +0800

    add init support for s3 and object_storage

---
+++ src/storage/object_storage/mod.rs
@@ -0,0 +1,3 @@
+pub mod s3;
+
+pub trait ObjectStorage {}
---
+++ src/storage/object_storage/s3.rs
@@ -0,0 +1,23 @@
+extern crate aws_sdk_rust;
+
+use aws_sdk_rust::aws::common::credentials::DefaultCredentialsProvider;
+use aws_sdk_rust::aws::common::region::Region;
+use aws_sdk_rust::aws::s3::endpoint::{Endpoint, Signature};
+use aws_sdk_rust::aws::s3::s3client::S3Client;
+use super::ObjectStorage;
+
+struct AwsS3Client {
+    bucket_name: String,
+}
+
+struct S3Config {
+    access_key_id: String,
+    secret_access_key: String,
+    bucket_name: String,
+    endpoint_url: String,
+    use_https: bool,
+    use_v4_signature: u8,
+    region_name: String,
+}
+
+impl ObjectStorage for AwsS3Client {}
